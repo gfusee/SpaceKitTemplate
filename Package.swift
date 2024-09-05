157d9919005525411c686924be2c6f8f6d2dec8b
quentin <quentus123@gmail.com>
2024-09-05 15:14:54 +0200

    renamed fields in Package.swift to make them easily replaceable

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Adder",
+    name: "##PACKAGE_NAME##",
     platforms: [
         .macOS(.v14)
     ],
@@ -16,7 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Adder",
+            name: "##TARGET_NAME##",
             dependencies: [
                 .product(name: "Space", package: "Space")
             ],
