19fbc9797dfd987314482153b7278ac48af41cbd
quentin <quentus123@gmail.com>
2024-11-29 14:31:10 +0100

    updated to 0.0.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [],
     dependencies: [
-        .package(url: "https://github.com/gfusee/SpaceKit.git", exact: "0.0.2")
+        .package(url: "https://github.com/gfusee/SpaceKit.git", exact: "0.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
