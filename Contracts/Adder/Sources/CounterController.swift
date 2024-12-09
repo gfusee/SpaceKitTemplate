3435d533a2b337ae40721141b8f159704635b4f1
quentin <quentus123@gmail.com>
2024-12-09 18:29:18 +0100

    fix

--- Contracts/Adder/Sources/CounterController.swift
+++ Contracts/Adder/Sources/CounterController.swift
@@ -1,11 +1,5 @@
 import SpaceKit
 
-@Init func initialize(initialValue: BigUint) {
-    var controller = CounterController()
-    
-    controller.counter = initialValue
-}
-
 @Controller public struct CounterController {
     @Storage(key: "counter") var counter: BigUint
     
---
+++ Contracts/Adder/Sources/init.swift
@@ -0,0 +1,5 @@
+@Init func initialize(initialValue: BigUint) {
+    var controller = CounterController()
+    
+    controller.counter = initialValue
+}
